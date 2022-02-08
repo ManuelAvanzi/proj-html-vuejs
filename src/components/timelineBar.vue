3a09d7899e87803cce2a4ec58c3e65a1857e7ab4
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-08 13:35:34 +0100

    header-timeline 1.0

--- src/components/timelineBar.vue
+++ src/components/timelineBar.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="container">
-        <span>starts TOMORROW! Our biggest event of the year...<i class="far fa-clock"></i>np</span>
+        <span>starts TOMORROW! Our biggest event of the year...
+            <i class="far fa-clock"></i>
+            <span class="time-zero">00:00:00:00</span>
+            <button class="button">get ticket</button>
+        </span>
     </div>
 </template>
 
@@ -15,11 +19,23 @@ export default {
 <style lang="scss" scoped>
 .container{
     width:100%;
-    height: 70px;
+    height: 40px;
     background-color: rgba(128, 128, 128, 0.863);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
 
     span{
         color:rgb(70, 70, 70);
+        margin:0 20px;
+    }
+    button{
+        color:white;
+        padding:4px 10px;
+        background-color: orange;
+        border-radius: 5px;
+        border-color:none;
     }
 }
 </style>
