503c587fc40afe6a7eab854a96c6c643a77cf509
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-10 16:12:31 +0100

    main E style scss

--- src/components/mainE.vue
+++ src/components/mainE.vue
@@ -1,13 +1,18 @@
 <template>
      <div class="section-container">
-
+         
+       
         <div class="center-text">
             <span class="sign">Article and Tips</span>
             <h1>Latest From The Blog</h1>
         </div>
 
-        <div class="cards-container">
-          <div class="main-card">
+        <div class="section-main">        
+             
+            <img  class="dots"  src="../assets/images/maxcoach-shape-12.png">
+            <img  class="line"  src="../assets/images/maxcoach-shape-03.png">   
+
+            <div class="main-card">
               <div class="text-container">
                 <div class="span">ARTIST</div>
                 <h3>Connection Between self-Portrait and identity</h3>
@@ -15,10 +20,46 @@
                 <i class="fa-solid fa-eye"></i><span>397 views</span>
               </div>
           </div>
+
+          <div id="left" class="small-card">
+              <div class="picture-container">
+
+              </div>
+              <div class="info-container">
+                <h4>ARTIST</h4>
+                <h3>Brush strokes Energize Trees in Paintings</h3>
+                <i class="fa-regular fa-calendar"></i><span>May 15, 2020</span>
+                <i class="fa-solid fa-eye"></i><span>397 views</span>
+              </div>
+          </div>
+          
+          <div id="right" class="small-card">
+              <div class="picture-container">
+
+              </div>
+              <div class="info-container">
+                <h4>ARTIST</h4>
+                <h3>Pocket sized notebooks hold miniature paintings</h3>
+                <i class="fa-regular fa-calendar"></i><span>May 15, 2020</span>
+                <i class="fa-solid fa-eye"></i><span>397 views</span>
+              </div>
+          </div>
+
+
+        <div class="details-container">
+             <span class="grey-text">Get in to details now ? </span>
+             <span class="red-text">View all posts ? </span>
+             <i class="fas fa-arrow-right"></i>
+         </div>
+
+
+        </div> 
+        
+    
         
             
-        </div>
-             
+        
+        
     </div>
 </template>
 
@@ -32,40 +73,5 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/style/variables.scss';
-.section-container{
-    width: 100%;
-    height: 950px;
-    
-    background-color: rgba(177, 167, 167, 0.452);
-
-    .cards-container{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        .main-card{
-            display: flex;
-            .text-container{
-                align-self: flex-end;
-            }
-
-        width: 25%;
-        height: 500px;
-        
-        background-image: url('../assets/images/artist-blog-02-500x680.jpg');
-        background-size: cover;
-
-        color:white;
-        
-
-    }
-
-    
-        
-    }
-    .center-text{
-        margin:20px auto;
-        text-align: center;
-    }
-}
+@import '@/style/mainE.scss';
 </style>
