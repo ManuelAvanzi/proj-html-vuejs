fd35697f87f01a968cf1c2fc001fe971a1c51ba2
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-02-10 10:52:59 +0100

    headerJubo - second brush

--- src/components/headerJumbo.vue
+++ src/components/headerJumbo.vue
@@ -4,7 +4,13 @@
             
             <img src="../assets/images/artist-hero-image-02.jpg" class="hero-image first" alt="">
             <img src="../assets/images/artist-hero-image-01.jpg" class="hero-image second" alt="">
-            <img src="../assets/images/artist-hero-image-04.jpg" class="hero-image third" alt="">
+            <img src="../assets/images/artist-hero-image-04.jpg" class="hero-image guy" alt="">
+            <img src="../assets/images/artist-hero-image-03.png" class=" face" alt="">
+            <img src="../assets/images/artist-shape-04.png" class="shape piuma" alt="">
+            <img src="../assets/images/artist-shape-03.png" class="shape rotate dots" alt="">
+            <img src="../assets/images/artist-shape-02.png" class="shape left-top" alt="">
+            <img src="../assets/images/artist-shape-01.png" class="shape leaf" alt="">
+            <img src="../assets/images/artist-shape-05.png" class="shape colors" alt="">
 
             <div class="text-container">
                 <div class="hello"><span class="sign">Hello, im Matin</span></div>
@@ -30,9 +36,47 @@ export default {
 @import '@/style/variables.scss';
    .jumbotron-container{
        width: 100%;
-       height: 450px;
-       background-color: pink;
+       height: 700px;
+       border:1px solid black;
+       background-color: #fbf9f6;
+       position:relative;
        
+       .left-top{
+           position:absolute;
+           top:0;
+           left:0;
+       }
+       .piuma{
+           position:absolute;
+           top:40px;
+           width: 100px;
+           right:130px;
+       }
+       .leaf{
+           position:absolute;
+           top:0;
+           right:120px;
+           width:400px;
+       }
+       .face{
+           position:absolute;
+           bottom:10px;
+           left:290px;
+           width: 150px;
+       }
+      
+       .dots{
+           position:absolute;
+           left:30px;
+           top:60px;
+           width: 200px;
+           transform: rotateY('90');
+       }
+       .colors{
+           position:absolute;
+           bottom:0;
+           right:20px;
+       }
 
        .text-container{
            border:1px solid grey;
@@ -51,9 +95,10 @@ export default {
        .hero-image{
            position:relative;
            width: 180px;
+            box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
        }
        .first{
-           
+           z-index: 1231;
            top:50px;
            left:300px;
        }
@@ -61,9 +106,11 @@ export default {
            top:200px;
            left:10px;
        }
-       .third{
+       .guy{
+           z-index: 1231;
            top:180px;
            left:650px;
+           width:270px;
        }
    
    
